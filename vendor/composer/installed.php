70f2d2340b7d964f23053d9bae06529779ec55f2
EdShel <eduardchelemetjev@gmail.com>
2020-06-01 19:07:09 +0300

    Added composer.lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '9999999-dev',
+    ),
+    'reference' => '93209464efc6b6a5fe5ae5766d184a49c2fc8460',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '93209464efc6b6a5fe5ae5766d184a49c2fc8460',
+    ),
+  ),
+);
